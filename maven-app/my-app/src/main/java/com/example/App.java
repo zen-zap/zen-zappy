27d4885b7b641e50c26e30bb48e6242529a78732
Ashutosh-Panda-340 <pandaashutosh340@gmail.com>
2024-09-08 17:28:40 +0530

    final commit

--- maven-app/my-app/src/main/java/com/example/App.java
+++ maven-app/my-app/src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, Ashu! Your Jenkins pipeline is working perfectly!!!!");
+        System.out.println("Hello, Ashu! Your Jenkins pipeline is working perfectly!!!! Making the Video!");
     }
 }
